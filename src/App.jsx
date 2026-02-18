7f9a559671060b24c7491f840586215af8592299
shah-unity-dev <shah.unity.dev@gmail.com>
2026-02-18 23:54:20 +0500

    del things

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
 // import Contact from './components/Contact'
-import Footer from './components/Footer'
-import Work from './components/Work'
-import Experience from './components/Experience'
-import About from './components/About'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import LenisScroll from './components/LenisScroll'
 
 export default function App() {
     return (
         <>
-            <LenisScroll />
-            <Navbar />
-            <Header />
-            <About />
-            <Experience />
-            <Work />
-            {/* <Contact /> */}
-            <Footer />
+            <h1>hello world</h1>
         </>
     )
 }
