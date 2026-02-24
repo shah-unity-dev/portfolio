39288a1157f2213a9dc26b1766709781a5465a25
shah-unity-dev <shah.unity.dev@gmail.com>
2026-02-24 20:07:54 +0500

    1.0.5

--- src/components/LenisScroll.jsx
+++ src/components/LenisScroll.jsx
@@ -2,27 +2,34 @@ import { useEffect } from "react";
 import Lenis from "lenis";
 
 export default function LenisScroll() {
-    useEffect(() => {
-        const lenis = new Lenis({
-            duration: 1.2,
-            smoothWheel: true,
-            smoothTouch: false,
-            anchors: {
-                offset: -100,
-            },
-        });
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+      smoothTouch: false,
+      anchors: {
+        offset: -100,
+      },
+    });
 
-        const raf = (time) => {
-            lenis.raf(time);
-            requestAnimationFrame(raf);
-        };
+    const raf = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
 
-        requestAnimationFrame(raf);
+    requestAnimationFrame(raf);
 
-        return () => {
-            lenis.destroy();
-        };
-    }, []);
+    const hash = window.location.hash;
+    if (hash) {
+      setTimeout(() => {
+        lenis.scrollTo(hash);
+      }, 300);
+    }
 
-    return null;
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return null;
 }
